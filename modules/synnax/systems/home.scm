210249d3e3d41511058d095f8c09347509d628e1
Karl Hallsby <karl@hallsby.com>
2026-05-24 12:08:05 -0500

    systems: home: Update nixpkgs channel to nixos-26.05.

--- modules/synnax/systems/home.scm
+++ modules/synnax/systems/home.scm
@@ -320,7 +320,7 @@ PROMPT_COMMAND=\"color_prompt_command${PROMPT_COMMAND:+;$PROMPT_COMMAND}\"")
                    home-files-service-type
                    (list `(".nix-channels"
                            ,(plain-file "nix-channels"
-                                        "https://nixos.org/channels/nixos-25.11 nixpkgs"))
+                                        "https://nixos.org/channels/nixos-26.05 nixpkgs"))
                          `(".config/nixpkgs/config.nix"
                            ,(plain-file "config.nix"
                                         "{
