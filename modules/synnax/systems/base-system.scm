587cd5923684754a9c1bc8c7e17ca4b3442d2371
Karl Hallsby <karl@hallsby.com>
2026-06-02 22:16:44 -0500

    systems: base: Fix indentation.

--- modules/synnax/systems/base-system.scm
+++ modules/synnax/systems/base-system.scm
@@ -38,172 +38,172 @@
     (keyboard-layout "us" #:options '("ctrl:nocaps")))
    (host-name "synnax-base-system")
    (users (cons* (user-account
-                   (name "karljoad")
-                   (comment "Karl Hallsby")
-                   (group "users")
-                   (home-directory "/home/karljoad")
-                   (supplementary-groups
-                    `("wheel" "netdev" "audio" "video"
-                      "kvm" "libvirt" "docker" "cgroup"
-                      "dialout" "plugdev")))
-                  %base-user-accounts))
-    (groups (cons* (user-group (name "plugdev")
-                               (system? #t))
-                   %base-groups))
-    (packages
-     (append
-      %system-packages
-      %base-packages))
-    (services
-     (append
-      (list (service plasma-desktop-service-type)
-            (service openssh-service-type)
-            (service cups-service-type
-                     (cups-configuration
-                      (web-interface? #t)))
-            (service fstrim-service-type)
-            (set-xorg-configuration
-             (xorg-configuration
-              (keyboard-layout keyboard-layout)))
-            ;; Provide a dictionary globally on my systems that I can look up
-            ;; from the command-line or from within Emacs.
-            ;; By default, this includes a major set of FreeDict dictionaries
-            ;; which covers a good number of languages I use.
-            (service dicod-service-type
-                     (dicod-configuration
-                       (handlers (list
-                                  (dicod-handler
-                                   (name "wordnet-handler")
-                                   (module "wordnet")
-                                   (options
-                                    (list #~(string-append "wnhome=" #$wordnet))))
-                                  (dicod-handler
-                                   (name "moby-thesaurus-handler")
-                                   (module "dictorg")
-                                   (options
-                                    (list #~(string-append "dbdir="
-                                                           #$(file-append dict-moby-thesaurus
-                                                                          "/share/dictd/")))))))
-                       (databases
-                        (cons* (dicod-database
-                                (name "wordnet")
-                                (complex? #t)
-                                (handler "wordnet-handler"))
-                               (dicod-database
-                                (name "moby-thesaurus")
-                                (complex? #t)
-                                (handler "moby-thesaurus-handler"))
-                               %dicod-database:gcide
-                               %dicod-databases:freedict))))
-            (service libvirt-service-type
-                     (libvirt-configuration
-                      (unix-sock-group "libvirt")))
-            (service virtlog-service-type)
-            (service containerd-service-type)
-            (service docker-service-type)
-            (service rootless-podman-service-type
-                     (rootless-podman-configuration
-                      (podman #f) ;; Users should grab podman themselves
-                      (containers-registries
-                       (mixed-text-file
-                          "podman-registries.conf"
-                          "unqualified-search-registries = ["
-                          (string-join
-                             (map (lambda (url) (format #f "'~a'" url))
-                                  (list "docker.io" "registry.fedoraproject.org"
-                                        "registry.opensuse.org"
-                                        "registry.access.redhat.com"
-                                        "registry.centos.org"))
-                                  ", ")
-                          "]"))
-                      (subuids
-                       (list
-                        (subid-range (name "karljoad")
-                                     (start 165536))))
-                      (subgids
-                       (list
-                        (subid-range (name "karljoad")
-                                     (start 165536))))))
-            (service nix-service-type
-                     (nix-configuration
-                      (extra-config
-                       (list "experimental-features = nix-command flakes\n"
-                             "trusted-users = root @wheel karljoad\n"
-                             ;; When using flakes, "nix shell" will not change $PS1.
-                             ;; This configuration should make "nix shell" behave
-                             ;; like "nix-shell" in setting $PS1.
-                             "bash-prompt = \\n\\[\\033[1;32m\\][nix-develop:\\w]\\$\\[\\033[0m\\]\\040\n"))))
-            (service syncthing-service-type
-                     (syncthing-configuration
-                      (user "karljoad") ;; TODO: Refactor `user' field to use variable.
-                      (arguments '("--gui-address=127.0.0.1:8384"))))
-            ;; 4-core VM with RTC set to Jan 2020, using Skylake for building
-            ;; Does NOT run by default. Must explicitly turn on for offload
-            ;; building to work!
-            (service virtual-build-machine-service-type)
-            ;; Allow for native compilation of foreign architectures. This is
-            ;; like cross-compilation, but instead of using a compiler for a
-            ;; TARGET architecture, my computer "fakes" being the building SYSTEM
-            ;; architecture.
-            ;; This must be manually enabled/disabled. You can control this with
-            ;; herd start/stop qemu-binfmt
-            (service qemu-binfmt-service-type
-                     (qemu-binfmt-configuration
-                      (platforms
-                       (lookup-qemu-platforms "arm" "aarch64" "riscv64"))))
-            ;; Enable stem darkening to make thin fonts look crisper at small resolutions.
-            ;; This is added to /etc/environment through session-environment-service-type
-            ;; which comes from (gnu system).
-            (simple-service
-             'font-stem-darkening-env-var session-environment-service-type
-             ;; We do not use (gnu home services)'s literal-string function here
-             ;; because I do not want the system to depend on guix home in any
-             ;; way, just for a clean separation of privileges. The format is
-             ;; used to wrap everything up into a set of double-quotes.
-             `(("FREETYPE_PROPERTIES" . ,(format #f "~s"
-                                          (string-join '("cff:no-stem-darkening=0"
-                                                         "cff:darkening-parameters=500,400,1000,350,1500,325,2000,300"
-                                                         "autofitter:no-step-darkening=0"
-                                                         "autofitter:darkening-parameters=500,400,1000,350,1500,325,2000,300"))))))
-            ;; Set the keyboard repeat rate in the CONSOLE. Setting it for X11
-            ;; must be done for X specifically. Setting the rate for Wayland
-            ;; is Wayland compositor-specific.
-            (service keyboard-console-service-type
-                     (keyboard-repeat-configuration
-                      (repeat-delay 200)
-                      (repeat-rate 40)))
-            ;; /bin/sh and /usr/bin/env are already made extra-special-files
-            ;; by %base-services, which %desktop-services extends
-            (extra-special-file "/bin/bash" (file-append bash "/bin/bash"))
-            ;; TODO: Add this?
-            ;; (name-service-switch %mdns-host-lookup-nss) ;; Resolve .local with mDNS
-            )
-      (modify-services %desktop-services
-                       (gdm-service-type config =>
-                                         (gdm-configuration
+                  (name "karljoad")
+                  (comment "Karl Hallsby")
+                  (group "users")
+                  (home-directory "/home/karljoad")
+                  (supplementary-groups
+                   `("wheel" "netdev" "audio" "video"
+                     "kvm" "libvirt" "docker" "cgroup"
+                     "dialout" "plugdev")))
+                 %base-user-accounts))
+   (groups (cons* (user-group (name "plugdev")
+                              (system? #t))
+                  %base-groups))
+   (packages
+    (append
+     %system-packages
+     %base-packages))
+   (services
+    (append
+     (list (service plasma-desktop-service-type)
+           (service openssh-service-type)
+           (service cups-service-type
+                    (cups-configuration
+                     (web-interface? #t)))
+           (service fstrim-service-type)
+           (set-xorg-configuration
+            (xorg-configuration
+             (keyboard-layout keyboard-layout)))
+           ;; Provide a dictionary globally on my systems that I can look up
+           ;; from the command-line or from within Emacs.
+           ;; By default, this includes a major set of FreeDict dictionaries
+           ;; which covers a good number of languages I use.
+           (service dicod-service-type
+                    (dicod-configuration
+                     (handlers (list
+                                (dicod-handler
+                                 (name "wordnet-handler")
+                                 (module "wordnet")
+                                 (options
+                                  (list #~(string-append "wnhome=" #$wordnet))))
+                                (dicod-handler
+                                 (name "moby-thesaurus-handler")
+                                 (module "dictorg")
+                                 (options
+                                  (list #~(string-append "dbdir="
+                                                         #$(file-append dict-moby-thesaurus
+                                                                        "/share/dictd/")))))))
+                     (databases
+                      (cons* (dicod-database
+                              (name "wordnet")
+                              (complex? #t)
+                              (handler "wordnet-handler"))
+                             (dicod-database
+                              (name "moby-thesaurus")
+                              (complex? #t)
+                              (handler "moby-thesaurus-handler"))
+                             %dicod-database:gcide
+                             %dicod-databases:freedict))))
+           (service libvirt-service-type
+                    (libvirt-configuration
+                     (unix-sock-group "libvirt")))
+           (service virtlog-service-type)
+           (service containerd-service-type)
+           (service docker-service-type)
+           (service rootless-podman-service-type
+                    (rootless-podman-configuration
+                     (podman #f) ;; Users should grab podman themselves
+                     (containers-registries
+                      (mixed-text-file
+                       "podman-registries.conf"
+                       "unqualified-search-registries = ["
+                       (string-join
+                        (map (lambda (url) (format #f "'~a'" url))
+                             (list "docker.io" "registry.fedoraproject.org"
+                                   "registry.opensuse.org"
+                                   "registry.access.redhat.com"
+                                   "registry.centos.org"))
+                        ", ")
+                       "]"))
+                     (subuids
+                      (list
+                       (subid-range (name "karljoad")
+                                    (start 165536))))
+                     (subgids
+                      (list
+                       (subid-range (name "karljoad")
+                                    (start 165536))))))
+           (service nix-service-type
+                    (nix-configuration
+                     (extra-config
+                      (list "experimental-features = nix-command flakes\n"
+                            "trusted-users = root @wheel karljoad\n"
+                            ;; When using flakes, "nix shell" will not change $PS1.
+                            ;; This configuration should make "nix shell" behave
+                            ;; like "nix-shell" in setting $PS1.
+                            "bash-prompt = \\n\\[\\033[1;32m\\][nix-develop:\\w]\\$\\[\\033[0m\\]\\040\n"))))
+           (service syncthing-service-type
+                    (syncthing-configuration
+                     (user "karljoad") ;; TODO: Refactor `user' field to use variable.
+                     (arguments '("--gui-address=127.0.0.1:8384"))))
+           ;; 4-core VM with RTC set to Jan 2020, using Skylake for building
+           ;; Does NOT run by default. Must explicitly turn on for offload
+           ;; building to work!
+           (service virtual-build-machine-service-type)
+           ;; Allow for native compilation of foreign architectures. This is
+           ;; like cross-compilation, but instead of using a compiler for a
+           ;; TARGET architecture, my computer "fakes" being the building SYSTEM
+           ;; architecture.
+           ;; This must be manually enabled/disabled. You can control this with
+           ;; herd start/stop qemu-binfmt
+           (service qemu-binfmt-service-type
+                    (qemu-binfmt-configuration
+                     (platforms
+                      (lookup-qemu-platforms "arm" "aarch64" "riscv64"))))
+           ;; Enable stem darkening to make thin fonts look crisper at small resolutions.
+           ;; This is added to /etc/environment through session-environment-service-type
+           ;; which comes from (gnu system).
+           (simple-service
+            'font-stem-darkening-env-var session-environment-service-type
+            ;; We do not use (gnu home services)'s literal-string function here
+            ;; because I do not want the system to depend on guix home in any
+            ;; way, just for a clean separation of privileges. The format is
+            ;; used to wrap everything up into a set of double-quotes.
+            `(("FREETYPE_PROPERTIES" . ,(format #f "~s"
+                                                (string-join '("cff:no-stem-darkening=0"
+                                                               "cff:darkening-parameters=500,400,1000,350,1500,325,2000,300"
+                                                               "autofitter:no-step-darkening=0"
+                                                               "autofitter:darkening-parameters=500,400,1000,350,1500,325,2000,300"))))))
+           ;; Set the keyboard repeat rate in the CONSOLE. Setting it for X11
+           ;; must be done for X specifically. Setting the rate for Wayland
+           ;; is Wayland compositor-specific.
+           (service keyboard-console-service-type
+                    (keyboard-repeat-configuration
+                     (repeat-delay 200)
+                     (repeat-rate 40)))
+           ;; /bin/sh and /usr/bin/env are already made extra-special-files
+           ;; by %base-services, which %desktop-services extends
+           (extra-special-file "/bin/bash" (file-append bash "/bin/bash"))
+           ;; TODO: Add this?
+           ;; (name-service-switch %mdns-host-lookup-nss) ;; Resolve .local with mDNS
+           )
+     (modify-services %desktop-services
+                      (gdm-service-type config =>
+                                        (gdm-configuration
+                                         (inherit config)
+                                         (wayland? #t)))
+                      (guix-service-type config =>
+                                         (guix-configuration
                                           (inherit config)
-                                          (wayland? #t)))
-                       (guix-service-type config =>
-                                          (guix-configuration
-                                           (inherit config)
-                                           (substitute-urls
-                                            (cons* "https://substitutes.nonguix.org"
-                                                   "https://nonguix-proxy.ditigal.xyz"
-                                                   (guix-configuration-substitute-urls config)))
-                                           (authorized-keys
-                                            (cons* keys/%nonguix
-                                                   (guix-configuration-authorized-keys config))))))))
-    (bootloader
-     (bootloader-configuration
-      (bootloader grub-efi-bootloader)
-      (targets '("/boot/efi"))
-      (keyboard-layout keyboard-layout)))
-    (file-systems
-     (append
-      (list (file-system
-              (device (file-system-label "fake-root"))
-              (mount-point "/")
-              (type "ext4")))
-      %base-file-systems))))
+                                          (substitute-urls
+                                           (cons* "https://substitutes.nonguix.org"
+                                                  "https://nonguix-proxy.ditigal.xyz"
+                                                  (guix-configuration-substitute-urls config)))
+                                          (authorized-keys
+                                           (cons* keys/%nonguix
+                                                  (guix-configuration-authorized-keys config))))))))
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (targets '("/boot/efi"))
+     (keyboard-layout keyboard-layout)))
+   (file-systems
+    (append
+     (list (file-system
+            (device (file-system-label "fake-root"))
+            (mount-point "/")
+            (type "ext4")))
+     %base-file-systems))))
 
 %base-system
