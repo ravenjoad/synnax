ravenjoad/synnax

commit b46eca9aa8ed68713b5865f035f67c97b43780b1

systems: website: Set Git HTTP path to just /.
We use nginx location rewrites to redirect HTTP Git queries around to
the spot fastcgi endpoint we expect, so we manually set the URI path
to "/", which will make HTTP Git operations look at "/srv/REPO.git"
on-disk.

See the git-http-configuration record in (guix) Version Control Services.

modules/synnax/systems/website.scm
@@ -325,7 +325,8 @@ if there is no matching extension."
                           ;; NOTE: git-http is for cloning using HTTP, not browsing!
                           ;; If you browse, you will always get a black webpage
                           (git-http-nginx-location-configuration
-                           (git-http-configuration))))
+                           (git-http-configuration
+                            (uri-path "/")))))
                         (raw-content
                          (list (nginx-hsts-header)
                                nginx-x-content-type-options-header

commit 2811fda6e07151a3c080042727fa8927e359f758

systems: website: Redirect /git/ paths to git's HTTP daemon.
git-http-nginx-location-configuration's uri-path is used as the
matcher in the generated nginx's location block. If it is set to "/",
then my website will have the following behavior:

  1. User browses to https://raven.hallsby.com with their Internet
     browser.
  2. Nginx receives and runs the nginx http block top-down, matching
     against the raven.hallsby.com server first, does not match on my
     custom rules (images, scripts, cgit redirect, etc.). Then has a
     regexp that matches on "/" in "https://raven.hallsby.com/", which
     will include things like "/index.html", "/blog", "/resume".
     Essentially everything.
  3. The user's page request is forwarded off to the git-http cgi
     script. The script obviously cannot find "/blog" (or whatever the
     user requested), since those do not exist in git-root. So a 404
     is returned.

So a 404 is returned for EVERY SINGLE WEBPAGE on my HTTP website.

I also updated cgit's little clone banner to include those more
convenient URLs too.

This bug was introduced in b46eca9aa8ed68713b5865f035f67c97b43780b1,
which explicitly specified the uri-path, instead of relying on the
default which is also "/git/".

modules/synnax/systems/website.scm
@@ -321,12 +321,18 @@ By default, age defaults to 1 year."
                           (nginx-location-configuration
                            (uri "~ /cgit(/[^/\\s]+)(/[^\\s]+)?")
                            (body '("return 308 $scheme://cgit.raven.hallsby.com$1$2$is_args$query_string ;")))
-                          ;; NOTE: git-http is for cloning using HTTP, not browsing!
-                          ;; If you browse, you will always get a black webpage
-                          ;; or a 403.
+                          ;; NOTE: git-http is for cloning using HTTP, not
+                          ;; browsing! If you browse uri-path with your browser,
+                          ;; a 404 will always be returned.
                           (git-http-nginx-location-configuration
                            (git-http-configuration
-                            (uri-path "/")))))
+                             (git-root "/srv/git")
+                             ;; git-daemon will match against uri-path to
+                             ;; redirect the git clone to Git's HTTP cgi script.
+                             ;; NOTE: If you set this to "/", then every URL in
+                             ;; "raven.hallsby.com" will be redirected to the
+                             ;; script and a 404 will be returned.
+                             (uri-path "/git/")))))
                         (raw-content
                          (list (nginx-hsts-header)
                                nginx-x-content-type-options-header
@@ -385,8 +391,10 @@ By default, age defaults to 1 year."
                      (favicon "/share/cgit/favicon.ico")
                      (clone-url
                       '("git://raven.hallsby.com/$CGIT_REPO_URL"
+                        "https://raven.hallsby.com/git/$CGIT_REPO_URL"
                         "https://raven.hallsby.com/cgit/$CGIT_REPO_URL"
                         "git://cgit.raven.hallsby.com/$CGIT_REPO_URL"
+                        "https://cgit.raven.hallsby.com/git/$CGIT_REPO_URL"
                         "https://cgit.raven.hallsby.com/$CGIT_REPO_URL"))
                      (enable-commit-graph? #t)
                      (enable-follow-links? #t)